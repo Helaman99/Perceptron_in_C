05ff05d62668049d379c02cb0fe64b3475f28fc6
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2025-02-04 15:01:50 -0800

    `dot` function simplification
    Now the function will determine the length of the arrays without us having to pass the length.

--- functions.h
+++ functions.h
@@ -1,8 +1,9 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-int dot(const int *arr1, const int *arr2, int size) {
+int dot(const int *arr1, const int *arr2) {
     int result = 0;
+    int size = sizeof(arr1) / sizeof(arr1[0]);
 
     for (int i = 0; i < size; i++) {
         result += arr1[i] * arr2[i];
