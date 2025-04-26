a823b44c551a82cd7a5a4393649771119abc2cf8
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2025-04-25 19:41:44 -0700

    Dynamic network creation

--- main.c
+++ main.c
@@ -37,42 +37,77 @@ regression where we want the prediction to be continuous values.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "Layer.h"
 #include "functions.h"
 
 #define INPUT_FEATURES 5
+#define LINE_LENGTH 1024
 
 int main() {
     int input[INPUT_FEATURES] = {1, 2, 3, 4, 5};
 
-    int layerID = 1;
-    int neuronCount = 2;
-    int weightCount = 5;
+    int layerCount = getLineCount("C:\\Git\\Perceptron_in_C\\network.csv", LINE_LENGTH);
 
-    Layer layer1 = populateLayer(layerID, neuronCount, weightCount);
+    FILE* networkData = fopen("C:\\Git\\Perceptron_in_C\\network.csv", "r");
+    if (!networkData) {
+        perror("Error opening network file!");
+        return EXIT_FAILURE;
+    }
 
-    layerID = 2;
-    neuronCount = 2;
-    weightCount = 2;
+    char* currNum = NULL;
+    int id = 0;
+    int neuronCount = 0;
+    int weightCount = 0;
+    int** neurons = NULL;
+    Layer* hiddenLayers = (Layer*)malloc(layerCount * sizeof(Layer));
+    char line[LINE_LENGTH];
+    printf("Processing input file...\n");
+    while (fgets(line, sizeof(line), networkData)) {
+        printf("Importing line %s\n", line);
 
-    Layer layer2 = populateLayer(layerID, neuronCount, weightCount);
+        hiddenLayers[id].id = id + 1;
+        printf("Creating layer with ID %d\n", hiddenLayers[id].id);
 
-    Layer hiddenLayers[2] = {
-        layer1,
-        layer2
-    };
+        neuronCount = atoi(strtok(line, ","));
+        hiddenLayers[id].neuronCount = neuronCount;
+        printf("Number of neurons: %d\n", neuronCount);
 
-    int layerCount = sizeof(hiddenLayers) / sizeof(hiddenLayers[0]);
-    int *inputVector = input;
+        weightCount = atoi(strtok(NULL, ","));
+        hiddenLayers[id].weightCount = weightCount;
+        printf("Number of weights per neuron: %d\n", weightCount);
+
+        neurons = malloc(neuronCount * sizeof(int*));
+        for (int i = 0; i < neuronCount; i++) {
+            neurons[i] = malloc(weightCount * sizeof(int));
+            for (int j = 0; j < weightCount; j++) {
+                neurons[i][j] = atof(strtok(NULL, ","));
+                printf("Adding weight: %d\n", neurons[i][j]);
+            }
+        }
+        hiddenLayers[id].neurons = neurons;
+        
+        id++;
+    }
+
+    fclose(networkData);
+
+    printf("Finished processing input file.\nNetwork contents:\n\n");
+    for (int i = 0; i < layerCount; i++) {
+        printLayer(hiddenLayers[i]);
+    }
+    printf("\n");
+
+    int* inputLayer = input;
     int inputSize = INPUT_FEATURES;
-    int *output = NULL;
+    int* output = NULL;
 
     for (int i = 0; i < layerCount; i++) {
-        output = multiplyLayers(inputVector, inputSize, hiddenLayers[i]);
+        output = multiplyLayers(inputLayer, inputSize, hiddenLayers[i]);
         if (i > 0) {
-            free(inputVector);
+            free(inputLayer);
         }
-        inputVector = output;
+        inputLayer = output;
         inputSize = hiddenLayers[i].neuronCount;
         freeLayer(hiddenLayers[i]);
     }
@@ -84,5 +119,5 @@ int main() {
     printf("}\n");
     free(output);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
